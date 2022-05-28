d3937e63b5ab7c07320aeffab445dba5477d72a2
MissDine <enidnyatichi1212@gmail.com>
2022-05-28 17:09:43 +0300

    Feat:edit

--- src/components/Count.js
+++ src/components/Count.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
 
 class Count extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         count:0
+      }
+    }
+    increament (){
+        this.setState({count:this.state.count + 1})
+    }
   render() {
     return (
-      <div>Count</div>
+        <>
+            <div>Count - {this.state.count}</div>
+            <button onClick = {() => this.increament()}>Increament</button>
+        </>
     )
   }
 }
